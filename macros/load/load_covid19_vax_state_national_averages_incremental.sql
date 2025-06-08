049ef71c177e8204a3eca98bf40b003f53c394c6
jason4king21 <jason4king21@gmail.com>
2025-06-08 14:05:28 -0500

    Load Macros

---
+++ macros/load/load_covid19_vax_state_national_averages_incremental.sql
@@ -0,0 +1,40 @@
+
+{% macro load_covid19_vax_state_national_averages_incremental() %}
+
+    {% set list_files_query %}
+        LIST @my_stage/NH_CovidVaxAverages_*.csv;
+    {% endset %}
+
+    {% set file_list_results = run_query(list_files_query) %}
+
+    {% for row in file_list_results %}
+        {% set file_name = row[0] %}
+
+        {% set check_query %}
+            SELECT COUNT(*) FROM HEALTHCARE.RAW_LOADED_FILES WHERE FILE_NAME = '{{ file_name }}';
+        {% endset %}
+
+        {% set check_result = run_query(check_query) %}
+
+        {% if check_result.columns[0].values()[0] == 0 %}
+
+            {% set copy_sql %}
+                COPY INTO HEALTHCARE.RAW.COVID19_VAX_STATE_NATIONAL_AVERAGES
+                FROM @my_stage/{{ file_name }}
+                FILE_FORMAT = (TYPE = CSV FIELD_OPTIONALLY_ENCLOSED_BY='"' SKIP_HEADER=1);
+            {% endset %}
+
+            {% do run_query(copy_sql) %}
+
+            {% set insert_sql %}
+                INSERT INTO HEALTHCARE.RAW_LOADED_FILES (FILE_NAME, LOAD_TIMESTAMP, TARGET_TABLE)
+                VALUES ('{{ file_name }}', CURRENT_TIMESTAMP, 'HEALTHCARE.RAW.COVID19_VAX_STATE_NATIONAL_AVERAGES');
+            {% endset %}
+
+            {% do run_query(insert_sql) %}
+
+        {% endif %}
+
+    {% endfor %}
+
+{% endmacro %}
---
+++ macros/load/load_inspection_dates_incremental.sql
@@ -0,0 +1,40 @@
+
+{% macro load_inspection_dates_incremental() %}
+
+    {% set list_files_query %}
+        LIST @my_stage/NH_SurveyDates_*.csv;
+    {% endset %}
+
+    {% set file_list_results = run_query(list_files_query) %}
+
+    {% for row in file_list_results %}
+        {% set file_name = row[0] %}
+
+        {% set check_query %}
+            SELECT COUNT(*) FROM HEALTHCARE.RAW_LOADED_FILES WHERE FILE_NAME = '{{ file_name }}';
+        {% endset %}
+
+        {% set check_result = run_query(check_query) %}
+
+        {% if check_result.columns[0].values()[0] == 0 %}
+
+            {% set copy_sql %}
+                COPY INTO HEALTHCARE.RAW.INSPECTION_DATES
+                FROM @my_stage/{{ file_name }}
+                FILE_FORMAT = (TYPE = CSV FIELD_OPTIONALLY_ENCLOSED_BY='"' SKIP_HEADER=1);
+            {% endset %}
+
+            {% do run_query(copy_sql) %}
+
+            {% set insert_sql %}
+                INSERT INTO HEALTHCARE.RAW_LOADED_FILES (FILE_NAME, LOAD_TIMESTAMP, TARGET_TABLE)
+                VALUES ('{{ file_name }}', CURRENT_TIMESTAMP, 'HEALTHCARE.RAW.INSPECTION_DATES');
+            {% endset %}
+
+            {% do run_query(insert_sql) %}
+
+        {% endif %}
+
+    {% endfor %}
+
+{% endmacro %}
