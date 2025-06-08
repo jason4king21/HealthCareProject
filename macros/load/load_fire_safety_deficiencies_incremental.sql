c332ba0eea29b4f17393e4e2ccc7501c2a3bfd2b
jason4king21 <jason4king21@gmail.com>
2025-06-08 14:43:10 -0500

    Filter added

--- macros/load/load_fire_safety_deficiencies_incremental.sql
+++ macros/load/load_fire_safety_deficiencies_incremental.sql
@@ -2,7 +2,7 @@
 {% macro load_fire_safety_deficiencies_incremental() %}
 
     {% set list_files_query %}
-        LIST @HEALTHCARE.RAW.MY_STAGE/*/NH_FireSafetyCitations_*.csv;
+        LIST @HEALTHCARE.RAW.MY_STAGE;
     {% endset %}
 
     {% set file_list_results = run_query(list_files_query) %}
@@ -10,28 +10,32 @@
     {% for row in file_list_results %}
         {% set file_name = row[0] %}
 
-        {% set check_query %}
-            SELECT COUNT(*) FROM HEALTHCARE.RAW.RAW_LOADED_FILES WHERE FILE_NAME = '{{ file_name }}';
-        {% endset %}
+        {% if file_name.match('.*NH_FireSafetyCitations_.*\.csv') %}
 
-        {% set check_result = run_query(check_query) %}
-
-        {% if check_result.columns[0].values()[0] == 0 %}
-
-            {% set copy_sql %}
-                COPY INTO HEALTHCARE.RAW.FIRE_SAFETY_DEFICIENCIES
-                FROM @HEALTHCARE.RAW.MY_STAGE/{{ file_name }}
-                FILE_FORMAT = (TYPE = CSV FIELD_OPTIONALLY_ENCLOSED_BY='"' SKIP_HEADER=1);
+            {% set check_query %}
+                SELECT COUNT(*) FROM HEALTHCARE.RAW.RAW_LOADED_FILES WHERE FILE_NAME = '{{ file_name }}';
             {% endset %}
 
-            {% do run_query(copy_sql) %}
+            {% set check_result = run_query(check_query) %}
 
-            {% set insert_sql %}
-                INSERT INTO HEALTHCARE.RAW.RAW_LOADED_FILES (FILE_NAME, LOAD_TIMESTAMP, TARGET_TABLE)
-                VALUES ('{{ file_name }}', CURRENT_TIMESTAMP, 'HEALTHCARE.RAW.FIRE_SAFETY_DEFICIENCIES');
-            {% endset %}
+            {% if check_result.columns[0].values()[0] == 0 %}
 
-            {% do run_query(insert_sql) %}
+                {% set copy_sql %}
+                    COPY INTO HEALTHCARE.RAW.FIRE_SAFETY_DEFICIENCIES
+                    FROM @HEALTHCARE.RAW.MY_STAGE/{{ file_name }}
+                    FILE_FORMAT = (TYPE = CSV FIELD_OPTIONALLY_ENCLOSED_BY='"' SKIP_HEADER=1);
+                {% endset %}
+
+                {% do run_query(copy_sql) %}
+
+                {% set insert_sql %}
+                    INSERT INTO HEALTHCARE.RAW.RAW_LOADED_FILES (FILE_NAME, LOAD_TIMESTAMP, TARGET_TABLE)
+                    VALUES ('{{ file_name }}', CURRENT_TIMESTAMP, 'HEALTHCARE.RAW.FIRE_SAFETY_DEFICIENCIES');
+                {% endset %}
+
+                {% do run_query(insert_sql) %}
+
+            {% endif %}
 
         {% endif %}
 
--- macros/load/load_provider_information_incremental.sql
+++ macros/load/load_provider_information_incremental.sql
@@ -2,7 +2,7 @@
 {% macro load_provider_information_incremental() %}
 
     {% set list_files_query %}
-        LIST @HEALTHCARE.RAW.MY_STAGE/*/NH_ProviderInfo_*.csv;
+        LIST @HEALTHCARE.RAW.MY_STAGE;
     {% endset %}
 
     {% set file_list_results = run_query(list_files_query) %}
@@ -10,28 +10,32 @@
     {% for row in file_list_results %}
         {% set file_name = row[0] %}
 
-        {% set check_query %}
-            SELECT COUNT(*) FROM HEALTHCARE.RAW.RAW_LOADED_FILES WHERE FILE_NAME = '{{ file_name }}';
-        {% endset %}
+        {% if file_name.match('.*NH_ProviderInfo_.*\.csv') %}
 
-        {% set check_result = run_query(check_query) %}
-
-        {% if check_result.columns[0].values()[0] == 0 %}
-
-            {% set copy_sql %}
-                COPY INTO HEALTHCARE.RAW.PROVIDER_INFORMATION
-                FROM @HEALTHCARE.RAW.MY_STAGE/{{ file_name }}
-                FILE_FORMAT = (TYPE = CSV FIELD_OPTIONALLY_ENCLOSED_BY='"' SKIP_HEADER=1);
+            {% set check_query %}
+                SELECT COUNT(*) FROM HEALTHCARE.RAW.RAW_LOADED_FILES WHERE FILE_NAME = '{{ file_name }}';
             {% endset %}
 
-            {% do run_query(copy_sql) %}
+            {% set check_result = run_query(check_query) %}
 
-            {% set insert_sql %}
-                INSERT INTO HEALTHCARE.RAW.RAW_LOADED_FILES (FILE_NAME, LOAD_TIMESTAMP, TARGET_TABLE)
-                VALUES ('{{ file_name }}', CURRENT_TIMESTAMP, 'HEALTHCARE.RAW.PROVIDER_INFORMATION');
-            {% endset %}
+            {% if check_result.columns[0].values()[0] == 0 %}
 
-            {% do run_query(insert_sql) %}
+                {% set copy_sql %}
+                    COPY INTO HEALTHCARE.RAW.PROVIDER_INFORMATION
+                    FROM @HEALTHCARE.RAW.MY_STAGE/{{ file_name }}
+                    FILE_FORMAT = (TYPE = CSV FIELD_OPTIONALLY_ENCLOSED_BY='"' SKIP_HEADER=1);
+                {% endset %}
+
+                {% do run_query(copy_sql) %}
+
+                {% set insert_sql %}
+                    INSERT INTO HEALTHCARE.RAW.RAW_LOADED_FILES (FILE_NAME, LOAD_TIMESTAMP, TARGET_TABLE)
+                    VALUES ('{{ file_name }}', CURRENT_TIMESTAMP, 'HEALTHCARE.RAW.PROVIDER_INFORMATION');
+                {% endset %}
+
+                {% do run_query(insert_sql) %}
+
+            {% endif %}
 
         {% endif %}
 
