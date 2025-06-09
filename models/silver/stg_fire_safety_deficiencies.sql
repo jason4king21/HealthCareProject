58c4bca8f2ae7f711215d4c56da9047a74e64581
jason4king21 <jason4king21@gmail.com>
2025-06-09 18:15:10 -0500

    Silver layer

---
+++ models/silver/stg_fire_safety_deficiencies.sql
@@ -0,0 +1,10 @@
+{ config(
+    materialized='table',
+    tags=['silver', 'profiled']
+) }
+
+SELECT
+    -- Example mapping, replace with full column list
+    *,
+    CURRENT_TIMESTAMP AS ingestion_timestamp
+FROM { source('raw', 'FIRE_SAFETY_DEFICIENCIES') }
